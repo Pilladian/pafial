b7b42314dd0a894adc9309f49ce7aef5c37493bb
Philipp Zimmermann <github@usrmail.de>
2020-04-06 17:08:20 +0200

    Added help() function for showing the usage of labso.jar

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,6 +1,13 @@
 public class Controller {
 
     public static void main(String[] args) {
+        // argument handling
+        if (args.length != 1) {
+            help();
+            System.exit(1);
+        }
+        // get path of labyrinth
+        String path = args[0];
 
         // read in labyrinth
 
@@ -11,4 +18,10 @@ public class Controller {
         // draw shortest way trough the labyrinth
     }
 
+    private static void help() {
+        System.out.println("Correct usage:\n\tjava -jar labso.jar </path/to/labyrinth.png>");
+        System.out.println("\nDefault configuration of image files and labso.jar");
+        System.out.println("\tSize of Image-file = 500x500 pixel");
+        System.out.println("\tSize of Labyrinth = 25x25 squares");
+    }
 }

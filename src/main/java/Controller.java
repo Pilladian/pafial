e35ca11fa5c2c48afe3f40f860f508d67c66c405
Philipp Zimmermann <github@usrmail.de>
2020-04-06 16:42:17 +0200

    Organization of project

---
+++ src/main/java/Controller.java
@@ -0,0 +1,14 @@
+public class Controller {
+
+    public static void main(String[] args) {
+
+        // read in labyrinth
+
+        // prepare data for solver
+
+        // solve labyrinth (possible with live imaging of the process)
+
+        // draw shortest way trough the labyrinth
+    }
+
+}

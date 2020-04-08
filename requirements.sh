48efbf7804bfb01fe4b6d1baedf080d5a73b6c40
Philipp Zimmermann <github@usrmail.de>
2020-04-09 00:33:28 +0200

    Added : requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+# Requirements
+
+# Installation of pip and cmake on Debian Linux based Systems
+# sudo apt install python3-pip cmake
+
+# Installation of openblas, pip and cmake on Arch Linux based Systems
+# sudo pacman -Sy openblas python-pip cmake
+
+
+# Installing actual python packages
+pip3 install opencv-python numpy scipy face_recognition pathlib dlib==19.15.0
